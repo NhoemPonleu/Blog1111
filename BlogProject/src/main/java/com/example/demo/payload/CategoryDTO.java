b6b1752f22dc344f2993a56b8354e5a4d7f29de1
ponleunhoem@gmail.com <ponleunhoem@gmail.com>
2022-11-02 15:41:24 +0700

    user_messageIn_catDTO

--- BlogProject/src/main/java/com/example/demo/payload/CategoryDTO.java
+++ BlogProject/src/main/java/com/example/demo/payload/CategoryDTO.java
@@ -14,9 +14,9 @@ import lombok.Setter;
 public class CategoryDTO {
    private Integer categoryID;
    @NotBlank
-   @Size(min = 4)
+   @Size(min = 4,message = "min size of category is 4")
    private String categoryTitle;
    @NotBlank
-   @Size(max = 10)
+   @Size(min = 10,message = "min size of cat is 10")
    private String categoryDescription;
 }
